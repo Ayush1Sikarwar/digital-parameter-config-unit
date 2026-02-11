21cda0475a18297daea11e46e048b7274aa528ee
Ayush1Sikarwar <ayushlegend1212@gmail.com>
2026-02-11 13:55:56 +0530

    add Oled_address in main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,12 @@
 #include <Arduino.h>
+#include <Wire.h>
+#include <Adafruit_SSD1306.h>
+#include <Adafruit_GFX.h>
+
+#define SCREEN_WIDTH 128
+#define SCREEN_HEIGHT 64
+#define OLED_ADDR 0x3C
+
 void setup() {
 // write your initialization code here
 }
